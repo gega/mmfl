0ce9c73d3c318d4478c4b288a2f3ab11c49fc993
Gergely Gati <gati.gergely@gmail.com>
2021-11-14 22:30:41 -0800

    fixed an issue when fd is used async

--- mmfl.h
+++ mmfl.h
@@ -10,13 +10,15 @@
 
 typedef struct rb_s
 {
-  int bp;
+  long bp;
   int bsiz;
   int fd;
   char *buf;
   char *ep;
   long mlen;
   char zero;
+  int rdy;
+  char sep;
 } rb_t;
 
 /*
@@ -26,20 +28,20 @@ typedef struct rb_s
  * \n len <space> message
  */
 
-#define rb_init(r,b,s,f) do { bzero((r),sizeof(struct rb_s)); (r)->buf=(b); (r)->bsiz=(s); (r)->mlen=-1; (r)->fd=(f); } while(0)
+#define rb_init(r,b,s,f) do { bzero((r),sizeof(struct rb_s)); (r)->buf=(b); (r)->bsiz=(s); (r)->mlen=-1; (r)->fd=(f); (r)->sep=1; } while(0)
 
 #define rb_readmsg(rs,rt,ln,rd) \
 do { \
-  char sep=1; \
-  if((rs)->mlen>=0) { \
+  if((rs)->rdy) { \
     memmove((rs)->buf,&(rs)->buf[(rs)->mlen],(rs)->bp-(rs)->mlen); \
     (rs)->buf[0]=(rs)->zero; \
     (rs)->bp-=(rs)->mlen; \
     (rs)->mlen=-1; \
+    (rs)->rdy=0; \
   } \
   /* look for new message */ \
-  while(sep!='\n') { if(0>=(rd)((rs)->fd,&sep,1)) { sep=0; break; } } \
-  if(sep) { while(1) { \
+  while('\n'!=(rs)->sep) { if(0>=(rd)((rs)->fd,&((rs)->sep),1)) break; } \
+  if('\n'==(rs)->sep) { while(1) { \
     if((rs)->mlen<0) { \
       if(memchr((rs)->buf,' ',(rs)->bp)!=NULL) { \
         (rs)->mlen=strtol((rs)->buf,&(rs)->ep,10); \
@@ -56,6 +58,7 @@ do { \
       (rs)->zero=(rs)->buf[(rs)->mlen]; \
       (rs)->buf[(rs)->mlen]='\0'; \
       (rt)=(rs)->ep+1; \
+      (rs)->rdy=1; \
       break; \
     } \
   } } else { (rt)=NULL; (ln)=-1; } \
