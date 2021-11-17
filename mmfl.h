b0446c65c9499f572129c418dd68fc59e9992294
Gergely Gati <gati.gergely@gmail.com>
2021-11-17 01:44:33 -0800

    optimized for larger read chunks and more tests added

--- mmfl.h
+++ mmfl.h
@@ -8,6 +8,7 @@
 #include <errno.h>
 
 
+
 typedef struct rb_s
 {
   long bp;
@@ -28,9 +29,9 @@ typedef struct rb_s
  * \n len <space> message
  */
 
-#define rb_init(r,b,s,f) do { bzero((r),sizeof(struct rb_s)); (r)->buf=(b); (r)->bsiz=(s); (r)->mlen=-1; (r)->fd=(f); (r)->sep=1; } while(0)
+#define RB_INIT(r,b,s,f) do { bzero((r),sizeof(struct rb_s)); (r)->buf=(b); (r)->bsiz=(s)-1; (r)->mlen=-1; (r)->fd=(f); } while(0)
 
-#define rb_readmsg(rs,rt,ln,rd) \
+#define RB_READMSG(rs,rt,ln,rd) \
 do { \
   if((rs)->rdy) { \
     memmove((rs)->buf,&(rs)->buf[(rs)->mlen],(rs)->bp-(rs)->mlen); \
@@ -38,16 +39,39 @@ do { \
     (rs)->bp-=(rs)->mlen; \
     (rs)->mlen=-1; \
     (rs)->rdy=0; \
+    (rs)->sep=0; \
   } \
-  /* look for new message */ \
-  while('\n'!=(rs)->sep) { if(0>=(rd)((rs)->fd,&((rs)->sep),1)) break; } \
-  if('\n'==(rs)->sep) { while(1) { \
-    if((rs)->mlen<0) { \
-      if(memchr((rs)->buf,' ',(rs)->bp)!=NULL) { \
+  while(1) { \
+    if((rs)->mlen<0&&0==(rs)->sep) { \
+      char *n=memchr((rs)->buf,'\n',(rs)->bp); \
+      if(NULL!=n) { \
+        memmove((rs)->buf,&(rs)->buf[n-(rs)->buf+1],(rs)->bp-(n-(rs)->buf+1)); \
+        (rs)->bp-=n-(rs)->buf+1; \
+        (rs)->sep=1; \
+      } else { \
+        (rs)->bp=0; \
+        (rs)->mlen=-1; \
+      } \
+    } \
+    if((rs)->mlen<0&&0!=(rs)->sep) { \
+      char *s=memchr((rs)->buf,' ',(rs)->bp); \
+      if(s!=NULL) { \
         (rs)->mlen=strtol((rs)->buf,&(rs)->ep,10); \
-        (ln)=(rs)->mlen; \
-        (rs)->mlen+=(rs)->ep-(rs)->buf+1; \
-      } else { (ln)=-1; } \
+        if((rs)->mlen>0) { \
+          (ln)=(rs)->mlen; \
+          (rs)->mlen+=(rs)->ep-(rs)->buf+1; \
+        } else { \
+          (rs)->mlen=-1; \
+          char *n=memchr((rs)->buf,'\n',(rs)->bp); \
+          if(NULL!=n&&n<s) s=n; \
+          memmove((rs)->buf,&(rs)->buf[s-(rs)->buf+1],(rs)->bp-(s-(rs)->buf+1)); \
+          (rs)->bp-=s-(rs)->buf+1; \
+          continue; \
+        } \
+      } else { \
+        (rs)->mlen=-1; \
+        (rs)->sep=1; \
+      } \
     } \
     if((rs)->mlen<0||(rs)->bp<(rs)->mlen) { \
       int len=(rd)((rs)->fd,&(rs)->buf[(rs)->bp],((rs)->bsiz-(rs)->bp)); \
@@ -61,7 +85,7 @@ do { \
       (rs)->rdy=1; \
       break; \
     } \
-  } } else { (rt)=NULL; (ln)=-1; } \
+  } \
 } while(0)
 
 #endif
--- t1.c
+++ t1.c
@@ -8,31 +8,95 @@
 #include "mmfl.h"
 
 
-int main(void)
+static int mock_read_count[]=
+{
+  5,4,5,3,4,3,5,4,2,3,3,1,3,2,5,5,1,2,3,3,1,1,2,4,2,3,1,3,5,4,2,4,5,4,1,5,3,1,4,5,3,
+  5,1,1,3,5,2,2,5,2,2,3,1,5,4,1,1,1,1,1,1,2,4,5,5,4,5,5,2,2,1,3,2,3,4,1,2,2,4,1,3,1,
+  2,2,4,3,1,4,3,3,3,3,3,4,2,1,1,2,4,1,
+  -1
+};
+static int mock_read_idx=0;
+
+
+static ssize_t mock_read(int fd, void *buf, size_t count)
+{
+  int cn;
+  
+  cn=mock_read_count[++mock_read_idx];
+  if(mock_read_count[mock_read_idx]<0) mock_read_idx=0;
+  return(read(fd,buf,(cn<count?cn:count)));
+}
+
+static int bufsizes[]={ 15, 140, 5, -1 };
+
+
+int main(int argc, char **argv)
 {
   rb_t rbv;
-  char buf[14];
+  char *buf;
   char *msg;
   int len,fd;
   char fn[32];
-  int i;
+  char rn[32];
+  int i,j,ts=0,k;
+  char rdb[200];
+  FILE *f;
 
-  for(i=0;;i++)
+  if(argc>1) ts=atoi(argv[1]);
+  for(j=0;bufsizes[j]>0;j++)
   {
-    snprintf(fn,sizeof(fn),"test%d.txt",i);
-    if(0>(fd=open(fn,0,O_RDONLY))) break;
-    printf("%stesting '%s'\n",(i==0?"":"\n"),fn);
+    printf("tests for buffer size %d\n",bufsizes[j]);
+    buf=calloc(1,bufsizes[j]);
+    if(NULL!=buf)
+    {
+      for(i=ts;;i++)
+      {
+        snprintf(fn,sizeof(fn),"test%d.txt",i);
+        snprintf(rn,sizeof(rn),"test%d.exp",i);
+        if(0>(fd=open(fn,0,O_RDONLY))) break;
+        if(NULL==(f=fopen(rn,"r"))) break;
+        printf("testing '%s'\n",fn);
 
-    rb_init(&rbv,buf,sizeof(buf),fd);
+        for(k=0;k<10;k++)
+        {
+          mock_read_idx=k;
+          
+          rewind(f);
+          lseek(fd,0,SEEK_SET);
 
-    do {
-      rb_readmsg(&rbv,msg,len,read);
-      if(msg==NULL&&len>=0) perror("message truncated or buffer too small");
-      else if(msg==NULL&&len<0) perror("eof or error");
-      else printf("message: [%d] '%s'\n",len,msg);
-    } while(msg!=NULL);
-    
-    close(fd);
+          RB_INIT(&rbv,buf,bufsizes[j],fd);
+
+          do {
+            RB_READMSG(&rbv,msg,len,mock_read);          
+            if(msg==NULL&&len>=0)
+            {
+               //printf("END\n");
+               ;
+            }
+            else if(msg==NULL&&len<0) perror("eof or error");
+            else 
+            {
+              if(NULL==(fgets(rdb,sizeof(rdb)-1,f)))
+              {
+                printf("ERROR: missing expected string!\n");
+                exit(1);
+              }
+              rdb[strlen(rdb)-1]='\0';
+              if(strcmp(rdb,msg)!=0)
+              {
+                printf("FAIL: read '%s' instead of '%s'\n",msg,rdb);
+              }
+              //else printf("OK\n");
+            }
+          } while(msg!=NULL);
+        }
+        
+        fclose(f);
+        close(fd);
+        if(argc>1) break;
+      }
+      free(buf);
+    }
   }
 
   return(0);
