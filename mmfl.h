ebd2e45ac03622b00bab35ce550a9186f93e00d2
Gergely Gati <gati.gergely@gmail.com>
2026-05-18 19:22:22 -0700

    peek function added to get the length of the next message without reading it fulley

--- mmfl.h
+++ mmfl.h
@@ -60,6 +60,8 @@
 
 #define MMFL_HDR_MAX (3+(MMFL_TYPE_DIGITS(MMFL_FD_TYPE,MMFL_LEN_BASE)))
 
+#define MMFL_PEEK (1<<0)
+
 
 _Static_assert(MMFL_LEN_BASE>=2 && MMFL_LEN_BASE<=36, "length base must be between 2 and 36");
 
@@ -83,10 +85,18 @@ typedef struct rb_s
  * \n len <space> message
  */
 
-#define RB_INIT(r,b,s,f) do { bzero((r),sizeof(struct rb_s)); (r)->buf=(b); (r)->bsiz=(s)-1; (r)->mlen=-1; (r)->fd=(f); } while(0)
+#define RB_INIT(r,b,s,f) do { \
+    bzero((r),sizeof(struct rb_s)); \
+    if(s>MMFL_HDR_MAX) { \
+      bzero((r),sizeof(struct rb_s)); (r)->buf=(b); (r)->bsiz=(s)-1; (r)->mlen=-1; (r)->fd=(f); \
+    } \
+  } while(0)
 
-#define RB_READMSG(rs,rt,ln,rd) \
+#define RB_IS_INITED(r) ((r)->buf!=NULL)
+
+#define RB_READMSG(rs,rt,ln,rd,pk) \
 do { \
+  if((rs)->buf==NULL) break; \
   if((rs)->rdy) { \
     memmove((rs)->buf,&(rs)->buf[(rs)->mlen],(rs)->bp-(rs)->mlen); \
     (rs)->buf[0]=(rs)->zero; \
@@ -114,6 +124,7 @@ do { \
         if((rs)->mlen>0) { \
           (ln)=(rs)->mlen; \
           (rs)->mlen+=(rs)->ep-(rs)->buf+1; \
+          if((pk)) break; \
         } else { \
           (rs)->mlen=-1; \
           char *n=memchr((rs)->buf,'\n',(rs)->bp); \
